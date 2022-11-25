d71acd1bc028927b8fd4b0a5de243bb33f454c8c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-11-25 08:17:24 -0800

    Revert "Add uv lines."
    This reverts commit e47f8d41c48fec02f4720474349bf922975fe25a.

--- script/DrawUVTex.gd
+++ script/DrawUVTex.gd
@@ -2,16 +2,47 @@ extends Node
 
 class_name DrawUVTex
 
+class Edge:
+	var a: Vector2
+	var b: Vector2
+
+#FIXME: Extremely slow...
 func draw_uv_texture(mesh: Mesh) -> PackedVector2Array:
+	var edges: Array[Edge]
 	var uvLines: PackedVector2Array
 	
 	for si in mesh.get_surface_count():
-		var mesh_data_tool : MeshDataTool = MeshDataTool.new()
-		mesh_data_tool.create_from_surface(mesh, si)
-		for edge_i in mesh_data_tool.get_edge_count():
-			for vertex_i in range(2):
-				var vertex = mesh_data_tool.get_edge_vertex(edge_i, vertex_i)
-				var uv = mesh_data_tool.get_vertex_uv(vertex)
-				uvLines.push_back(uv)
+		var a = mesh.surface_get_arrays(si)
+		
+		var uv:PackedVector2Array = a[Mesh.ARRAY_TEX_UV]
+		var indices = a[Mesh.ARRAY_INDEX]
+		
+		var ic:int
+		var useIndices = false
+		
+		if indices.size() > 0:
+			ic = indices.size()
+			useIndices = true
+		else:
+			ic = uv.size()
+		
+		for i in range(0, ic, 3):
+			for j in range(3):
+				var edge: Edge = Edge.new()
+				
+				if useIndices:
+					edge.a = uv[indices[i + j]]
+					edge.b = uv[indices[i + ((j + 1) % 3)]]
+				else:
+					edge.a = uv[i + j]
+					edge.b = uv[i + ((j + 1) % 3)]
+				
+				if edges.has(edge):
+					continue
+				
+				uvLines.push_back(edge.a)
+				uvLines.push_back(edge.b)
+				
+				edges.append(edge)
 		
 	return uvLines
