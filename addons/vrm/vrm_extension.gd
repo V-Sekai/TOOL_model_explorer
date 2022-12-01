dfe79f28c1d333e3344e5317f4939d029349c373
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-12-01 11:51:40 -0800

    Revert to the last working vrmimporter.

--- addons/vrm/vrm_extension.gd
+++ addons/vrm/vrm_extension.gd
@@ -125,12 +125,12 @@ func skeleton_rename(gstate : GLTFState, p_base_scene: Node, p_skeleton: Skeleto
 func rotate_scene_180_inner(p_node: Node3D, mesh_set: Dictionary, skin_set: Dictionary):
 	if p_node is Skeleton3D:
 		for bone_idx in range(p_node.get_bone_count()):
-			var rest: Transform3D = p_node.get_bone_rest(bone_idx) * ROTATE_180_TRANSFORM
+			var rest: Transform3D = ROTATE_180_TRANSFORM * p_node.get_bone_rest(bone_idx) * ROTATE_180_TRANSFORM
 			p_node.set_bone_rest(bone_idx, rest)
-			p_node.set_bone_pose_rotation(bone_idx, p_node.get_bone_pose_rotation(bone_idx) * Quaternion(ROTATE_180_BASIS))
+			p_node.set_bone_pose_rotation(bone_idx, Quaternion(ROTATE_180_BASIS) * p_node.get_bone_pose_rotation(bone_idx) * Quaternion(ROTATE_180_BASIS))
 			p_node.set_bone_pose_scale(bone_idx, Vector3.ONE)
 			p_node.set_bone_pose_position(bone_idx, rest.origin)
-	p_node.transform = p_node.transform * ROTATE_180_TRANSFORM
+	p_node.transform = ROTATE_180_TRANSFORM * p_node.transform * ROTATE_180_TRANSFORM
 	if p_node is ImporterMeshInstance3D:
 		mesh_set[p_node.mesh] = true
 		skin_set[p_node.skin] = true
@@ -973,6 +973,7 @@ func _import_preflight(gstate : GLTFState, psa=PackedStringArray(), psa2: Varian
 
 func apply_retarget(gstate : GLTFState, root_node: Node, skeleton: Skeleton3D, bone_map: BoneMap) -> Array[Basis]:
 	var skeletonPath: NodePath = root_node.get_path_to(skeleton)
+
 	skeleton_rename(gstate, root_node, skeleton, bone_map)
 	var hips_bone_idx = skeleton.find_bone("Hips")
 	if hips_bone_idx != -1:
